7a07e20ca4862b5037c09c4c063d566521e5d0e5
Laurent Loiseau <lt.loiseau@gmail.com>
2022-02-03 19:46:37 +0100

    renamed app class + cleaning

--- src/main/java/io/java_web_base/JavaWebBase.java
+++ src/main/java/io/java_web_base/JavaWebBase.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InfoRMeApp extends SpringBootServletInitializer {
+public class JavaWebBase extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(InfoRMeApp.class, args);
+        SpringApplication.run(JavaWebBase.class, args);
 
     }
 
